8fa1d9dc7657a84af1c12298d74cb0ad32aeb369
Rodrimm06 <rodrimm2006@gmail.com>
2026-04-21 16:09:19 -0600

    Se agrego colores por label

--- tarea-3/src/components/Contact.jsx
+++ tarea-3/src/components/Contact.jsx
@@ -5,8 +5,9 @@ function Contact({contact_name, number, label, onDelete }) {
     <div className="flex  items-center justify-between gap-3 p-3 border rounded-lg mb-2 b-5 p-5 m-5">
       <h2 className="text-lg font-bold font-inter">{contact_name}</h2>
       <p className="text-gray-600 font-inter p-4">{number}</p>
-      <p className="text-white bg-blue-500 rounded-full font-inter p-4">{label}</p>
-      <button className="text-white bg-red-500 rounded-full font-inter px-4 py-2 " onClick={onDelete}>X</button>
+      <p className={`text-white rounded-full font-inter p-4 
+        ${label === "Amigo" ? "bg-teal-500" : label==="Trabajo" ? "bg-cyan-500" : label==="Familia" ?"bg-sky-500" :"bg-blue-800"}`}>{label}</p>
+      <button className="text-white bg-red-600 rounded-full font-inter px-4 py-2 " onClick={onDelete}>X</button>
     </div>
   );
 }
