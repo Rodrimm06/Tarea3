a9b8b6dc1c71b1dba748f1fa12d88486da73ebd2
Rodrimm06 <rodrimm2006@gmail.com>
2026-04-21 16:28:20 -0600

    Se agregaron fuentes en negrita para los labels

--- tarea-3/src/components/Contact.jsx
+++ tarea-3/src/components/Contact.jsx
@@ -5,7 +5,7 @@ function Contact({contact_name, number, label, onDelete }) {
     <div className="flex  items-center justify-between gap-3 p-3 border rounded-lg mb-2 b-5 p-5 m-5">
       <h2 className="text-lg font-bold font-inter">{contact_name}</h2>
       <p className="text-gray-600 font-inter p-4">{number}</p>
-      <p className={`text-white rounded-full font-inter p-4 
+      <p className={`text-white rounded-full font-inter p-4 font-bold
         ${label === "Amigo" ? "bg-teal-500" : label==="Trabajo" ? "bg-cyan-500" : label==="Familia" ?"bg-sky-500" :"bg-blue-800"}`}>{label}</p>
       <button className="text-white bg-red-600 rounded-full font-inter px-4 py-2 " onClick={onDelete}>X</button>
     </div>
